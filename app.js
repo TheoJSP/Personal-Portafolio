ee2e9436524dcd3454669379f06631520738a9e6
Sp <41648020+TheoJSP@users.noreply.github.com>
2020-10-15 19:19:09 -0300

    Scroll, footer and skills

--- app.js
+++ app.js
@@ -6,7 +6,20 @@ $(document).ready(()=>{
         }else{
             $(".navbar").removeClass("sticky")
         }
+
+        if(this.scrollY > 500){
+            $(".scroll-up-btn").addClass("show")
+        }else{
+            $(".scroll-up-btn").removeClass("show")
+        }
+    
+
     });
+    //SLIDE-UP 
+    $(".scroll-up-btn").click(()=>{
+        $("html").animate({scrollTop: 0});
+    });
+    
     //NAVBAR SCRIPT
     $(".menu-btn").click(()=>{
         $(".navbar .menu").toggleClass("active");
@@ -20,5 +33,12 @@ $(document).ready(()=>{
         backSpeed: 60,
         loop: true
     });
+    var typed = new Typed(".typing2", {
+        strings: ["Developer", "Student"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
 
 })
